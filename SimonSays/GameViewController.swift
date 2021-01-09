02ff86bf7c759b0192677e3c650468a5918291b2
Alessandro Alterno <alternonolimit@gmail.com>
2021-01-09 04:45:56 +0100

    Added score. When loosing you come back to the main page.

--- SimonSays/GameViewController.swift
+++ SimonSays/GameViewController.swift
@@ -15,6 +15,10 @@ class GameViewController: UIViewController, CBPeripheralDelegate {
     
     var actionToSend: Data?
     
+    var startGame = true
+    @IBOutlet weak var scoreValueLabel: UILabel!
+    
+    
     init(peripheral: CBPeripheral) {
         
         self.peripheral = peripheral
@@ -30,6 +34,12 @@ class GameViewController: UIViewController, CBPeripheralDelegate {
         peripheral.delegate = self
         peripheral.discoverServices([Constants.SERVICE_UUID])
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let serv = self.service {
+            peripheral.discoverCharacteristics([Constants.CHARAC_UUID], for: serv)
+        }
     }
 
     @IBAction func touchYellowButton(_ sender: Any) {
@@ -69,20 +79,27 @@ class GameViewController: UIViewController, CBPeripheralDelegate {
         if let characteristic = service.characteristics?.first(where: { $0.uuid == Constants.CHARAC_UUID }) {
             peripheral.setNotifyValue(true, for: characteristic)
             self.characteristic = characteristic
+            if startGame {
+                let startMsg = Data("start".utf8)
+                peripheral.writeValue(startMsg, for: characteristic, type: CBCharacteristicWriteType.withoutResponse)
+                startGame = false
+            }
             if let actionToSend = self.actionToSend {
                 peripheral.writeValue(actionToSend, for: characteristic, type: CBCharacteristicWriteType.withoutResponse) // Send the msg to the device
-                
-                print(peripheral.readValue(for: characteristic))
             }
-             
-            
+            peripheral.readValue(for: characteristic)
         }
     }
 
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         if let recievedValue = characteristic.value {
-            let str = String(decoding: recievedValue, as: UTF8.self)
-            print(str)
+            let recievedMsg = String(decoding: recievedValue, as: UTF8.self)
+            if recievedMsg.contains("scr:") {
+                let extractedValue = recievedMsg.components(separatedBy: ":")
+                print("ok -> \(extractedValue[1])")
+            } else if recievedMsg == "lost" {
+                dismiss(animated: false)
+            }
         }
     }
 }
